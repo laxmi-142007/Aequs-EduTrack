9e1e676cfe03af95ee89ec84ef1d706ec88f964e
Code-Cool-2006 <rchavadar@gmail.com>
2026-09-04 10:10:16 +0530

    feat: import JsonResponse to enable JSON-based report responses

--- reports/views.py
+++ reports/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from students.models import Student
 from eligibility.models import EligibilityRecord, BenefitType
 from reports.models import ActivityLog, log_activity
